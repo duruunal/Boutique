2d2cb84f3dfd3ab027c2f22f88152c0830fb19bc
Duru Unal <duruunal@gmail.com>
2022-06-03 01:56:03 -0400

    Initial Commit

---
+++ Boutique/BoutiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoutiqueApp.swift
+//  Boutique
+//
+//  Created by Student on 2022-06-03.
+//
+
+import SwiftUI
+
+@main
+struct BoutiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
